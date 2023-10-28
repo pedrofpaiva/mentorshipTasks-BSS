18a8873cced0a6656927795ac99bc8bd6cbbd930
Pedro <paivapedroportoalegre@gmail.com>
2023-10-28 19:59:09 -0300

    divisibilty check for 3 exercice

---
+++ Exercices/while.ts
@@ -0,0 +1,12 @@
+// Divisibility Check for 3
+
+let i = 0
+
+while ( i < 10000) {
+  if (i % 3 === 0) {
+    console.log(`${i} é divisível por 3.`)
+  } else {
+    console.log(`${i} não é divisível por 3.`)
+  }
+  i = i+1
+ }
